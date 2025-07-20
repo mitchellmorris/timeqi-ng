d3344ed0c15b3c8fd6b1d40f00d95c88030f0beb
Mitchell <mitchellwaynemorris@gmail.com>
2025-07-20 06:55:35 -0700

    feat: `ng add @ngxs/store` and friends

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config'; 
+import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { authInterceptor } from './providers/auth/authInterceptor';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,9 +20,13 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([authInterceptor])
     ),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
-    })
+      theme: {
+        preset: Aura
+      }
+    }), 
+    provideStore(
+      [],
+      withNgxsReduxDevtoolsPlugin()
+    )
   ]
 };
