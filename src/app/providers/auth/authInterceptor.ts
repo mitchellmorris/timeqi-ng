b2d86809ee26511fecb0da7d68056c5679936d51
Mitchell <mitchellwaynemorris@gmail.com>
2025-08-08 08:00:52 -0700

    feat(auth): unauthorized login redirect

--- src/app/providers/auth/authInterceptor.ts
+++ src/app/providers/auth/authInterceptor.ts
@@ -1,18 +1,29 @@
-import { HttpEvent, HttpHandler, HttpHandlerFn, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
 import { Observable } from "rxjs/internal/Observable";
 
 export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const idToken = localStorage.getItem("id_token");
+  const router = inject(Router); // Create a new instance of Router
   if (idToken) {
     const cloned = req.clone({
       headers: req.headers.set("Authorization",
         "Bearer " + idToken
       )
     });
-    return next(cloned);
-  }
-  else {
+    // return next(cloned);
+    return next(cloned).pipe(
+          catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+              // Redirect to login page
+              router.navigate(['/login']);
+            }
+            return throwError(error); // Re-throw the error for other handlers
+          })
+        );
+  } else {
     return next(req);
   }
 }
