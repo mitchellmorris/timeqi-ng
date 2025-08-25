423b7cab971b26bce2d94da7542d7a0a33df2870
Mitchell <mitchellwaynemorris@gmail.com>
2025-08-25 11:11:25 -0700

    feat(task): add crude scheduling

--- src/app/pages/task/task-settings/task-scheduling/task-scheduling.ts
+++ src/app/pages/task/task-settings/task-scheduling/task-scheduling.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Store } from '@ngxs/store';
+import { OrganizationsState } from '../../../../store/organizations/organizations.state';
+import { SchedulingSettings, Task } from '@betavc/timeqi-sh';
+import { StateUtils } from '../../../../providers/utils/state';
+import { first, Observable } from 'rxjs';
+import { WorkshiftSchedular } from '../../../../components/workshift-schedular/workshift-schedular';
+import { SaveOrganizationSchedule } from '../../../../store/organizations/organizations.actions';
+import { TasksState } from '../../../../store/tasks/tasks.state';
 
 @Component({
   selector: 'app-task-scheduling',
-  imports: [],
+  imports: [
+    WorkshiftSchedular
+  ],
+  providers: [
+    StateUtils
+  ],
   templateUrl: './task-scheduling.html',
   styleUrl: './task-scheduling.css'
 })
 export class TaskScheduling {
+  readonly store = inject(Store);
+  readonly route = inject(ActivatedRoute);
+  readonly stateUtils = inject(StateUtils);
+  id = this.route.snapshot.params['organizationId'];
+  task$: Observable<Task> = this.stateUtils.getState$(
+    TasksState.getState, 
+    'task'
+  ).pipe(
+    first((task): task is Task => task !== null)
+  );
+  task = toSignal<SchedulingSettings>(this.task$, { initialValue: null });
 
+  onSubmit(formData: SchedulingSettings) {
+    console.log('Form Data:', formData);
+    // this.store.dispatch(new SaveOrganizationSchedule(
+    //   this.id,
+    //   formData
+    // ));
+  }
 }
