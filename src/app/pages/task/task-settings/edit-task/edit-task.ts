3678eb5e42ebd739cb2524fd4fa038e23fc2f113
Mitchell <mitchellwaynemorris@gmail.com>
2025-11-17 16:08:22 -0800

    feat: clean up edit task form

--- src/app/pages/task/task-settings/edit-task/edit-task.ts
+++ src/app/pages/task/task-settings/edit-task/edit-task.ts
@@ -1,10 +1,10 @@
-import { Component, effect, inject, OnDestroy } from '@angular/core';
+import { Component, effect, inject, OnDestroy, Signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { SetTask } from '../../../../store/tasks/tasks.actions';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { TasksState } from '../../../../store/tasks/tasks.state';
-import { PartialUser, Task } from '@betavc/timeqi-sh';
+import { PartialUser, Task, PrimeNG } from '@betavc/timeqi-sh';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FluidModule } from 'primeng/fluid';
@@ -16,6 +16,7 @@ import { DatePipe } from '@angular/common';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { UserState } from '../../../../store/user/user.state';
 import { SelectModule } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-edit-task',
@@ -27,7 +28,8 @@ import { SelectModule } from 'primeng/select';
     SelectButtonModule,
     DatePipe,
     InputNumberModule,
-    SelectModule
+    SelectModule,
+    DatePickerModule
   ],
   templateUrl: './edit-task.html',
   styleUrl: './edit-task.css'
@@ -37,14 +39,9 @@ export class EditTask {
   readonly route = inject(ActivatedRoute);
   readonly fb = inject(FormBuilder);
   readonly stateUtils = inject(StateUtils);
-  taskId = this.route.snapshot.paramMap.get('taskId');
-  task$ = this.stateUtils.getState$(TasksState.getState, 'task');
-  task = toSignal(this.task$, { initialValue: null });
-  users$ = this.stateUtils.getState$(UserState.getState, 'users').pipe(
-    map(users => users.map((user: PartialUser) => ({ label: user.name, value: user._id }))),
-  );
-  users = toSignal(this.users$, { initialValue: [] });
-  modeOptions = [
+  task: Signal<Task | null> = this.store.selectSignal(TasksState.getTask);
+  users: Signal<PrimeNG.SelectOption[]> = this.store.selectSignal(UserState.getUserSelectOptions);
+  modeOptions: PrimeNG.SelectOption[] = [
     { label: 'Snap', value: false },
     { label: 'Lock', value: true }
   ];
@@ -59,14 +56,14 @@ export class EditTask {
   constructor(public router: Router) {
     effect(() => {
       if (this.task()) {
-        this.form.patchValue(this.task());
+        this.form.patchValue(this.task() as Task);
       }
     });
   }
   onSubmit() {
     if (this.form.valid) {
       const updatedTask: Task = {
-        ...this.task,
+        ...this.task(),
         ...this.form.value
       };
       console.log('Updated Task:', updatedTask);
